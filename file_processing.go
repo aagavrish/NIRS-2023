7eedcba4b478a53a50ed567fc21a43256617b24f
Alexander Gavrish <yagavrishaleksandr@gmail.com>
2023-04-21 14:50:06 +0300

    add data and go files

---
+++ file_processing.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"encoding/csv"
+	"encoding/json"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func merging_files(filenames []string) {
+	var index int = 0
+	for _, filename := range filenames {
+
+		file, err := os.Open(filename)
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+		reader := csv.NewReader(file)
+		reader.Comma = ';'
+
+		csvData, err := reader.ReadAll()
+		if err != nil {
+			panic(err)
+		}
+
+		for i := range csvData {
+			for j := range csvData[i] {
+				csvData[i][j] = strings.ReplaceAll(csvData[i][j], ",", ".")
+			}
+		}
+
+		var accident Accident
+		for _, each := range csvData {
+			accident.Number = index
+			accident.Date = each[1]
+			accident.District = each[2]
+			accident.DTPnumber = each[3]
+			accident.DTPform = each[4]
+			accident.Road = each[5]
+			accident.Address = each[8]
+			accident.Deapths, _ = strconv.ParseFloat(each[9], 32)
+			accident.Wounded, _ = strconv.ParseFloat(each[10], 32)
+			accident.CARScount, _ = strconv.ParseFloat(each[11], 32)
+			accident.MEMBERScount, _ = strconv.ParseFloat(each[12], 32)
+			accidents = append(accidents, accident)
+			index++
+		}
+	}
+}
+
+func parseJSON(filename string) {
+	jsonData, err := json.MarshalIndent(accidents, "", " ")
+	if err != nil {
+		panic(err)
+	}
+
+	jsonFile, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer jsonFile.Close()
+
+	jsonFile.Write(jsonData)
+}
+
+type Accident struct {
+	Number       int     `json:"Number"`
+	Date         string  `json:"Date"`
+	District     string  `json:"District"`
+	DTPnumber    string  `json:"DTPnumber"`
+	DTPform      string  `json:"DTPform"`
+	Road         string  `json:"Road"`
+	Address      string  `json:"Address"`
+	Deapths      float64 `json:"Deapths"`
+	Wounded      float64 `json:"Wounded"`
+	CARScount    float64 `json:"CARScount"`
+	MEMBERScount float64 `json:"MEMBERScount"`
+}
+
+var accidents []Accident
+
+func main() {
+	CSVfilenames := []string{
+		"./data/2021/January2021.csv", "./data/2021/February2021.csv", "./data/2021/March2021.csv",
+		"./data/2021/April2021.csv", "./data/2021/May2021.csv", "./data/2021/June2021.csv",
+		"./data/2021/July2021.csv", "./data/2021/August2021.csv", "./data/2021/September2021.csv",
+		"./data/2021/October2021.csv", "./data/2021/November2021.csv", "./data/2021/December2021.csv",
+
+		"./data/2022/January2022.csv", "./data/2022/February2022.csv", "./data/2022/March2022.csv",
+		"./data/2022/April2022.csv", "./data/2022/May2022.csv", "./data/2022/June2022.csv",
+		"./data/2022/July2022.csv", "./data/2022/August2022.csv", "./data/2022/September2022.csv",
+		"./data/2022/October2022.csv", "./data/2022/November2022.csv", "./data/2022/December2022.csv"}
+	jsonFilename := "./data/data.json"
+
+	merging_files(CSVfilenames)
+	parseJSON(jsonFilename)
+}
---
+++ forecast_calculation.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+const days = 365 * 2
+
+type Accident struct {
+	Number       int     `json:"Number"`
+	Date         string  `json:"Date"`
+	District     string  `json:"District"`
+	DTPnumber    string  `json:"DTPnumber"`
+	DTPform      string  `json:"DTPform"`
+	Road         string  `json:"Road"`
+	Address      string  `json:"Address"`
+	Deapths      float64 `json:"Deapths"`
+	Wounded      float64 `json:"Wounded"`
+	CARScount    float64 `json:"CARScount"`
+	MEMBERScount float64 `json:"MEMBERScount"`
+}
+
+type District struct {
+	Name         string
+	AccidentRate float64
+}
+
+var districts []District
+
+func districts_add_name(accidents []Accident) {
+	var district District
+	var district_names []string = district_export(accidents, 1)
+	for _, curname := range district_names {
+		district.Name = curname
+		district.AccidentRate = 0.0
+		districts = append(districts, district)
+	}
+}
+
+func districts_calculate_accidentrate(accidents []Accident, flag int) float64 {
+	districts_add_name(accidents)
+	var carsCount float64 = 0
+	var averageRate float64 = 0
+	for i := 0; i < len(districts); i++ {
+		for j := 0; j < len(accidents); j++ {
+			if districts[i].Name == accidents[j].District {
+				carsCount += accidents[j].CARScount
+			}
+		}
+		districts[i].AccidentRate = float64(int(carsCount/days*100*100)) / 100
+		if flag == 0 {
+			fmt.Printf("%d. %s: %.2f\n", i, districts[i].Name, districts[i].AccidentRate)
+		}
+		averageRate += districts[i].AccidentRate
+		carsCount = 0
+	}
+	averageRate /= float64(len(districts))
+	return averageRate
+}
+
+func district_export(accidents []Accident, flag int) []string {
+	var district_names []string
+	district_names = append(district_names, accidents[0].District)
+
+	for index := range accidents {
+		if unique_district(accidents[index].District, district_names) {
+			district_names = append(district_names, accidents[index].District)
+		}
+	}
+
+	if flag == 0 {
+		for index, districtname := range district_names {
+			fmt.Printf("%d. %s\n", index, districtname)
+		}
+	}
+
+	return district_names
+}
+
+func unique_district(curdistrict string, alldistricts []string) bool {
+	for _, districtname := range alldistricts {
+		if districtname == curdistrict {
+			return false
+		}
+	}
+	return true
+}
+
+func calculation(accidents []Accident) {
+	var averageRate float64 = districts_calculate_accidentrate(accidents, 1)
+	var count float64
+	var district string
+
+	fmt.Printf("Район: ")
+	fmt.Scan(&district)
+	for i := 0; i < len(districts); i++ {
+		if districts[i].Name == district {
+			for _, accident := range accidents {
+				if accident.District == district {
+					fmt.Printf("Дата: %s. Номер ДТП: %s. Форма ДТП: %s.\tКоличество авто: %.0f\n",
+						accident.Date, accident.DTPnumber, accident.DTPform, accident.CARScount)
+					count += accident.CARScount
+				}
+			}
+			fmt.Printf("Итого %d автомобилей попали в ДТП\n\n", int(count))
+			if districts[i].AccidentRate <= averageRate+10 && districts[i].AccidentRate >= averageRate-10 {
+				fmt.Printf("В районе '%s' СРЕДНЕЕ значение аварийности: %.2f%% (среднее значение: %.2f%%)\n", districts[i].Name, districts[i].AccidentRate, averageRate)
+			} else if districts[i].AccidentRate > averageRate+10 {
+				fmt.Printf("В районе '%s' ВЫСОКОЕ значение аварийности: %.2f%% (среднее значение: %.2f%%)\n", districts[i].Name, districts[i].AccidentRate, averageRate)
+			} else if districts[i].AccidentRate < averageRate-10 {
+				fmt.Printf("В районе '%s' НИЗКОЕ значение аварийности: %.2f%% (среднее значение: %.2f%%)\n", districts[i].Name, districts[i].AccidentRate, averageRate)
+			}
+		}
+	}
+}
+
+func main() {
+	JSONfile, err := os.Open("./data/data.json")
+	if err != nil {
+		panic(err)
+	}
+	defer JSONfile.Close()
+
+	byteResult, _ := ioutil.ReadAll(JSONfile)
+	var accidents []Accident
+	json.Unmarshal(byteResult, &accidents)
+
+	var flag int
+	fmt.Printf("1 - вывести список возможных районов\n2 - выбор района для прогнозирования\nФлаг: ")
+	fmt.Scan(&flag)
+
+	switch flag {
+	case 1:
+		district_export(accidents, 0)
+	case 2:
+		calculation(accidents)
+	case 3:
+		districts_calculate_accidentrate(accidents, 0)
+	default:
+		fmt.Println("Неккоректное значение флага")
+	}
+}
